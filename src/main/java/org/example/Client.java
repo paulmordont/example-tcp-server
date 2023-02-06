ba59685695d20950660b84ab79c365dfa57cc83b
Pavel Hubich <pavelh@spotify.com>
2023-02-06 23:35:33 +0100

    Initial commit

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+  private String host;
+  private int port;
+
+  Socket socket;
+
+  PrintWriter writer;
+
+  BufferedReader reader;
+
+  public Client(String host, int port) {
+    this.host = host;
+    this.port = port;
+  }
+
+  public void connect() throws IOException {
+    socket = new Socket(host, port);
+    writer = new PrintWriter(socket.getOutputStream(), true);
+    reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+  }
+
+  public String sendStringAndGetResponse(String data) {
+    try {
+      writer.println(data);
+      return reader.readLine();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public void close() throws IOException {
+    socket.close();
+  }
+}
---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Server {
+
+  private int port;
+
+  public Server(int port) {
+    this.port = port;
+  }
+
+  public void start() {
+  }
+
+  public void stop() {
+  }
+
+}
